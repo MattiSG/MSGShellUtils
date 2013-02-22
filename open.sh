e1cd570928a71a6a021a5c7fb023f9557c352017
Matti Schneider <matti.schneider@mesh-md.fr>
2013-02-22 10:54:55 +0100

    Add cross-platform `open`

---
+++ open.sh
@@ -0,0 +1,9 @@
+# Cross-platform Darwin open(1)
+# Simply add this function definition above any OSX script that uses the “open” command
+# For additional information on the “open” command, see https://developer.apple.com/library/mac/#documentation/darwin/reference/manpages/man1/open.1.html
+open() {
+	if [[ $(uname) = "Darwin" ]]
+	then /usr/bin/open "$@"	#OS X
+	else xdg-open "$@" &> /dev/null &	# credit: http://stackoverflow.com/questions/264395
+	fi
+}
